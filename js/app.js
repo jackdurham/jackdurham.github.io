d84c9137c1f10bf4e1ddd4acd2deb0ff9960d93d
Jack Durham <jackdurham11@gmail.com>
2018-02-26 17:24:03 +0000

    adding sc

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log(' new loaded');
+$(document).ready(function() {
+  $('#fullpage').fullpage();
+});
+
+function init() {
+
+  $(document).ready(function(){
+
+
+    $('#fullpage').fullpage({
+      anchors: ['about', 'projects', 'skills', 'contact']
+    });
+
+
+  });
+
+}
+
+
+init();
